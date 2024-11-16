9ca33887672fa9ce0112cc09b8d7c3b3346ae098
dragondaddy <dragondaddy@caselibre.fr>
2024-11-16 21:28:05 +0100

    Let's go

---
+++ conf/config-sample.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Configuration
+ *
+ * Usually, the wishthis installer will create the config.php for you.
+ */
+
+namespace wishthis;
+
+/**
+ * Database
+ *
+ * Used to connect to your MySQL database.
+ */
+define('DATABASE_HOST', 'localhost');
+define('DATABASE_NAME', '__DB_NAME__');
+define('DATABASE_USER', '__DB_USER__');
+define('DATABASE_PASSWORD', '__DB_PWD__');
+
+/**
+ * Development
+ *
+ * Keep this disabled for production sites.
+ */
+define('ENV_IS_DEV', false);
